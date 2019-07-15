f4e547a0b937f124ddbae6eb26e808db1143a918
DARYL HUANG (ITA-ISDC-ISD-OOCLL/ZHA) <HUANGDA7@OOCL.COM>
2019-07-15 21:46:16 +0800

    test:add content of test should_return_expense_type_A_if_project_is_external_and_name_is_project_A

--- src/test/java/ExpenseService/ExpenseServiceTest.java
+++ src/test/java/ExpenseService/ExpenseServiceTest.java
@@ -26,8 +26,13 @@ class ExpenseServiceTest {
     @Test
     void should_return_expense_type_A_if_project_is_external_and_name_is_project_A() throws UnexpectedProjectTypeException {
         // given
+        Project externalProjectA= new Project(ProjectType.EXTERNAL, "Project A");
+
         // when
+        ExpenseType expensetype=ExpenseService.getExpenseCodeByProjectTypeAndName(externalProjectA);
+
         // then
+        Assertions.assertEquals(ExpenseType.EXPENSE_TYPE_A,expensetype);
     }
 
     @Test
